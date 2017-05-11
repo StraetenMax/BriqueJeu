200f1714799010e591a01e9fd579e09b8f030dfe
StraetenMax <maxstraeten@yahoo.com>
2017-05-11 09:58:55 +0200

    ElementsGraphique propriétés constructeur acesseur

--- src/examCnam/ElementGraphique.java
+++ src/examCnam/ElementGraphique.java
@@ -1,5 +1,41 @@
 package examCnam;
 
 public class ElementGraphique {
-
+	//propriétés
+	private String nom;
+	private int hauteur, largeur;
+	
+	//constructeurs
+	public ElementGraphique(){
+		nom ="";
+		hauteur = 0;
+		largeur = 0;
+	}
+	public ElementGraphique(String nom, int hauteur, int largeur){
+		nom = this.nom;
+		hauteur = this.hauteur;
+		largeur = this.largeur;
+		}
+	//accesseurs et mutateurs
+	public String getNom(){
+		return nom;
+	}
+	public int getHauteur(){
+		return hauteur;
+	}
+	public int getLargeur(){
+		return largeur;
+	}
+	public void setNom(String yNom){
+		nom = yNom;
+	}
+	public void setHauteur(int yHauteur){
+		hauteur = yHauteur;
+	}
+	public void setLargeur(int yLargeur){
+		largeur = yLargeur;
+	}
+	//méthodes
+	
 }
+	
