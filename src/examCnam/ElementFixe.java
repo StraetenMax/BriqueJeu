085a53f54007c0e62589d38be3d2075db8ff58c0
StraetenMax <maxstraeten@yahoo.com>
2017-05-11 10:34:17 +0200

    ElementFixe accesseur mutateur methodes

--- src/examCnam/ElementFixe.java
+++ src/examCnam/ElementFixe.java
@@ -15,5 +15,27 @@ public class ElementFixe extends ElementGraphique {
 		positionX =cPositionX;
 		positionY =cPositionY;
 	}
+	//accesseurs et mutateurs
+	public int getPositionX(){
+		return positionX;
+	}
+	public int getPositionY(){
+		return positionY;
+	}
+	public void setPositionX(int zPositionX){
+		positionX = zPositionX;
+	}
+	public void setPositionY(int zPositionY){
+		positionY = zPositionY;
+	}
+	//méthodes
+	public String afficher(){
+		String str1 = "Elément fixe affiché";
+		return str1;
+	}
+	public String description(){
+		String str2 = super.description() +" qui est en position X "+this.getPositionX()+" et position Y "+this.getPositionY();
+		return str2;
+	}
 	
 }
